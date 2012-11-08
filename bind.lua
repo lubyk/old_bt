b0f76f2ed3f660c7c40fa09887cef764bd75db64
Gaspard Bucher <gaspard@teti.ch>
2012-11-08 11:19:56 +0100

    Basic Lua bindings.

--- bind.lua
+++ bind.lua
@@ -15,32 +15,53 @@ require 'lubyk'
 local base = lk.scriptDir()
 local bt_base = base .. '/src/vendor/bullet/src'
 
+-- 5 = show all warnings
+dub.warn_level = 4
+
 local ins = dub.Inspector {
   INPUT   = {
-    -- bt_base .. '/BulletCollision/BroadphaseCollision',
-    -- bt_base .. '/BulletCollision/CollisionDispatch',
-    -- bt_base .. '/BulletCollision/CollisionShapes',
-    -- bt_base .. '/BulletCollision/Gimpact',
-    -- bt_base .. '/BulletCollision/NarrowPhaseCollision',
-    --
-    -- bt_base .. '/BulletDynamics/Character',
-    -- bt_base .. '/BulletDynamics/ConstraintSolver',
-    -- bt_base .. '/BulletDynamics/Dynamics',
-    -- bt_base .. '/BulletDynamics/Vehicle',
-    
-    -- bt_base .. '/BulletSoftBody',
-    -- bt_base .. '/LinearMath',
+    bt_base .. '/BulletCollision/BroadphaseCollision/btBroadphaseInterface.h',
+    bt_base .. '/BulletCollision/BroadphaseCollision/btDbvtBroadphase.h',
+    bt_base .. '/BulletCollision/BroadphaseCollision/btDispatcher.h',
 
-    base .. '/bind/BulletCollision/BroadphaseCollision',
-    base .. '/bind/BulletCollision/CollisionDispatch',
-    base .. '/bind/BulletCollision/CollisionShapes',
-    base .. '/bind/BulletDynamics/ConstraintSolver',
-    base .. '/bind/BulletDynamics/Dynamics',
-    base .. '/bind/LinearMath',
+    bt_base .. '/BulletCollision/CollisionDispatch/btCollisionConfiguration.h',
+    bt_base .. '/BulletCollision/CollisionDispatch/btCollisionDispatcher.h',
+    bt_base .. '/BulletCollision/CollisionDispatch/btDefaultCollisionConfiguration.h',
+
+    bt_base .. '/BulletCollision/CollisionShapes/btCollisionShape.h',
+    bt_base .. '/BulletCollision/CollisionShapes/btConvexInternalShape.h',
+    bt_base .. '/BulletCollision/CollisionShapes/btConcaveShape.h',
+    bt_base .. '/BulletCollision/CollisionShapes/btConvexShape.h',
+    bt_base .. '/BulletCollision/CollisionShapes/btSphereShape.h',
+    bt_base .. '/BulletCollision/CollisionShapes/btStaticPlaneShape.h',
+
+    bt_base .. '/BulletDynamics/ConstraintSolver/btConstraintSolver.h',
+    bt_base .. '/BulletDynamics/ConstraintSolver/btSequentialImpulseConstraintSolver.h',
+
+    bt_base .. '/BulletDynamics/Dynamics/btDiscreteDynamicsWorld.h',
+    bt_base .. '/BulletDynamics/Dynamics/btDynamicsWorld.h',
+    bt_base .. '/BulletDynamics/Dynamics/btRigidBody.h',
+
+    bt_base .. '/LinearMath/btDefaultMotionState.h',
+    bt_base .. '/LinearMath/btMotionState.h',
+    bt_base .. '/LinearMath/btQuadWord.h',
+    bt_base .. '/LinearMath/btQuaternion.h',
+    bt_base .. '/LinearMath/btScalar.h',
+    bt_base .. '/LinearMath/btTransform.h',
+    bt_base .. '/LinearMath/btVector3.h',
   },
   PREDEFINED = '"SIMD_FORCE_INLINE=" "ATTRIBUTE_ALIGNED16(name)=name" "BT_DECLARE_ALIGNED_ALLOCATOR()="',
   ignore = {
-    -- FIXME: Why cannot dub ignore classes here ?
+    'btSelect',
+    'btSwapEndian',
+    'btSwapEndianDouble',
+    'btUnswapEndianDouble',
+    'btSwapEndianFloat',
+    'btUnswapEndianFloat',
+    btDbvtBroadphase = {
+      'm_sets',
+      'm_stageRoots',
+    },
   },
   -- html = true,
 }
@@ -63,10 +84,9 @@ end
 
 binder:bind(ins, {
   output_directory = base .. '/src/bind',
-  custom_bindings  = base .. '/bind',
   -- Remove this part in headers
-  -- LuaBinder 115
-  header_base = {bt_base, base .. '/bind'},
+  header_base = {bt_base},
+
   -- Execute all lua_open in a single go
   -- with luaopen_bt_vendor (creates bt_vendor.cpp).
   single_lib = 'bt',
